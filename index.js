30ed9a22eff18c87af5ab873eb5a2ecc140e1a10
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-05-24 18:06:33 +0100

    place holders for CSS, HTML and JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var tc = require("./language/typec");
 
+tc.CSS = require("./language/css");
+
+tc.HTML = require("./language/html");
+
+tc.JavaScript = require("./language/javascript");
+
 tc.JSON = require("./language/json");
 
 module.exports = tc;
